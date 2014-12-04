0c533b679e8dfc08c488acd55458a2959f8a69bf
Kleber Soares <kleberr@msn.com>
2014-12-04 20:28:41 -0300

    First commit

---
+++ evoluirmais/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+
+from django.contrib import admin
+admin.autodiscover()
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', 'evoluirmais.core.views.home', name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
